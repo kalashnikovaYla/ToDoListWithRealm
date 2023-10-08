92a5980bf2cb00ed02f19c8a12fe49a031c441ae
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-10-08 09:25:50 +0500

    Initial Commit

---
+++ ToDoListWithRealm/ToDoListWithRealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListWithRealmApp.swift
+//  ToDoListWithRealm
+//
+//  Created by sss on 08.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListWithRealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
